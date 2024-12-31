d4200f9daa6a9034d7135f98345d04dd85f50adc
mark <schnitzi@gmail.com>
2024-12-31 13:44:36 +0000

    Added 10K computation.

--- Strava5KCalculator.user.js
+++ Strava5KCalculator.user.js
@@ -11,20 +11,37 @@
 window.addEventListener('load', function() {
     'use strict';
 
+    function format(seconds) {
+        return Math.floor(seconds/60) + ":" + String(seconds % 60).padStart(2, '0')
+    }
+
+    function addRow(label, seconds) {
+        var tr = document.createElement("tr");
+        var td = document.createElement("td");
+        td.textContent = label + format(seconds);
+        tr.appendChild(td);
+        splitsRow.appendChild(tr);
+    }
+
     var seconds = 0;
+    var fiveKSeconds = 0;
     var splitsRow = document.getElementsByClassName('mile-splits')[0].children[0].children[1];
     var splits = splitsRow.children;
     var timeMatcher = /(\d+):(\d+)/;
-    for (var i=0; i<5; i++) {
+    for (var i=0; i<splits.length; i++) {
         var time = splits[i].children[1].textContent;
         var matcher = time.match(timeMatcher);
-        seconds += parseInt(matcher[1])*60 + parseInt(matcher[2]);
+        var split = parseInt(matcher[1])*60 + parseInt(matcher[2])
+        seconds += split;
+        if (i < 5) {
+            fiveKSeconds += split
+        }
     }
 
-    var tr = document.createElement("tr");
-    var td = document.createElement("td");
-    td.textContent = "5K time: " + Math.floor(seconds/60) + ":" + String(seconds % 60).padStart(2, '0');
-    tr.appendChild(td);
-    splitsRow.appendChild(tr);
-
+    if (splits.length >= 5) {
+        addRow("5K time: ", fiveKSeconds);
+        if (splits.length >= 10) {
+            addRow("10K time: ", seconds);
+        }
+    }
 }, false);
